d5f6a3f51e103df390c0a6971566c65cdf778474
Abeer Khakwani <thekfamily10@gmail.com>
2015-03-13 10:26:05 -0700

    Add home.twig and the result page

--- app/app.php
+++ app/app.php
@@ -3,6 +3,32 @@
     require_once __DIR__."/../src/RepeatCounter.php";
 
 
+   $app= new Silex\Application();
+
+   $app['debug'] = true;
+
+   $app->register(new Silex\Provider\TwigServiceProvider(), array(
+       'twig.path' => __DIR__.'/../views'
+   ));
+
+   $app->get("/", function() use ($app){
+           return $app['twig']->render('home.twig');
+       });
+
+
+   $app->post("/repeat_result", function() use ($app){
+
+
+               $input_word=$_POST['word'];
+               $input_string=$_POST['repeat']
+
+               $new_Repeat_counter= new RepeatCounter;
+
+              $result= $new_Repeat_counter->countRepeats($input_word,$input_string);
+
+
+               return $app['twig']->render('result.twig');
+           });
 
 
 
@@ -15,13 +41,6 @@
 
 
 
-
-
-
-
-
-
-
- return $app;    
+ return $app;
 
 ?>
