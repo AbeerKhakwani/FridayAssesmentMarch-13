13366cc58d6feccc26083373a07fdf7207073739
Abeer Khakwani <thekfamily10@gmail.com>
2015-03-13 09:47:17 -0700

    Test the function with three letter words and with no matches

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -15,5 +15,40 @@
             $this->assertEquals(1,$result);
         }
 
+        function test_Repeat_Counter_With_NoMatches()
+        {   //Arrange
+            $test_RepeatCounter= new RepeatCounter;
+            $input1 = "A";
+            $input2 = "b";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input1,$input2);
+            //Assert
+            $this->assertEquals(0,$result);
+        }
+
+        function test_Repeat_Counter_Word()
+        {   //Arrange
+            $test_RepeatCounter= new RepeatCounter;
+            $input1 = "the";
+            $input2 = "the";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input1,$input2);
+            //Assert
+            $this->assertEquals(1,$result);
+        }
+        function test_Repeat_Counter_Word_noMatches()
+        {   //Arrange
+            $test_RepeatCounter= new RepeatCounter;
+            $input1 = "the";
+            $input2 = "you";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input1,$input2);
+            //Assert
+            $this->assertEquals(0,$result);
+        }
     }
+
 ?>
